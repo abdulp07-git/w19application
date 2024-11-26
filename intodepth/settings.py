9eadf33bf6447b80f22ca5f3bcd7fdd952c5c925
Abdul <abdul@intodepth.in>
2024-11-27 01:38:48 +0530

    Static updated

--- intodepth/settings.py
+++ intodepth/settings.py
@@ -129,7 +129,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+#STATIC_URL = 'static/'
+STATIC_URL = 'https://d33frleuz6dkp9.cloudfront.net/'  # CloudFront URL
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
